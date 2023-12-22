fe28e4927f692f94363c61ae03c28eeab75fabb8
Georgiy <149246960+neomitt@users.noreply.github.com>
2023-12-22 18:28:15 +0600

    Add files via upload

---
+++ biograper/app/src/main/java/com/example/biograper/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.biograper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.button);
+
+    }
+    public void onClick(View v){
+        finish();
+
+    }
+}
